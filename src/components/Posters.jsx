4283f49baac28bed25b0253df2abfd357a346e93
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-08-05 23:54:34 +0700

    add more poster and implement zooming parameter as well as the cog for navigate between posters

--- src/components/Posters.jsx
+++ src/components/Posters.jsx
@@ -1,13 +1,25 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "../css/Posters.css";
 import LightButton from "./LightButton";
-import poster1 from "../assets/poster1.webp";
+import routine from "../assets/posters/routine.webp";
+import questions from "../assets/posters/Questions.png";
+import moss from "../assets/posters/Moss-final.png";
+import consuming from "../assets/posters/Consuming.png";
+import breath from "../assets/posters/Breathe-Ps.png";
+import porsche from "../assets/posters/Porsche.png";
+import yakult from "../assets/posters/Yakult-Ps.png";
 import rotate from "../assets/rotate.png";
+import wheel from "../assets/wheel.png";
 
 export default function Posters() {
   const [isLightOn, setIsLightOn] = useState(true);
   const [xAxis, setXAxis] = useState(0);
   const [yAxis, setYAxis] = useState(0);
+  const [containerYAxis, setContainerYAxis] = useState(0);
+  const [zoom, setZoom] = useState(0);
+  const xLimit = { min: -200, max: 200 };
+  const yLimit = { min: -300, max: 300 };
+  const maxZoom = 5;
 
   function toggleIsLightOn() {
     if (isLightOn) {
@@ -18,26 +30,93 @@ export default function Posters() {
     setIsLightOn(!isLightOn);
   }
 
+  useEffect(() => {
+    function updateClipPath() {
+      const element = document.querySelector(".poster-frame");
+      const rect = element.getBoundingClientRect();
+      const width = rect.width;
+      const height = rect.height;
+
+      // Scale the coordinates based on actual dimensions
+      const scaledPath = `path("M0,0 H${width} V${height} H0 Z M0,${
+        height * 0.3
+      } Q${width * 0.1},${height * 0.19} 0,${height * 0.1} Z")`;
+
+      element.style.clipPath = scaledPath;
+    }
+
+    updateClipPath();
+
+    // Update on load and resize
+    window.addEventListener("resize", updateClipPath);
+    return () => {
+      window.removeEventListener("resize", updateClipPath);
+    };
+  }, []);
+
+  const scale = 1 + zoom;
+  const posterStyle = {
+    transform: `scaleX(-${scale}) scaleY(${scale}) translateX(${xAxis}px) translateY(${yAxis}px)`,
+  };
+
   return (
-    <div className="poster-frame">
-      <div className="paper-border">
-        <div className="paper">
-          <img
-            src={poster1}
-            className="poster"
-            style={{
-              transform: `scaleX(-1) translateX(${xAxis}px) translateY(${yAxis}px)`,
-            }}
+    <div className="poster-wrapper">
+      <div className="poster-frame">
+        <div className="paper-border">
+          <div className="paper">
+            <div
+              className="poster-container"
+              style={{ transform: `translateY(-${containerYAxis}px)` }}
+            >
+              <div className="poster-viewer">
+                <img src={routine} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={questions} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={consuming} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={moss} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={breath} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={porsche} className="poster" style={posterStyle} />
+              </div>
+              <div className="poster-viewer">
+                <img src={yakult} className="poster" style={posterStyle} />
+              </div>
+            </div>
+          </div>
+          {!isLightOn && <div className="light-overlay"></div>}
+          {!isLightOn && <div className="light-shadow-overlay"></div>}
+        </div>
+        <LightButton isOn={isLightOn} toggleIsOn={toggleIsLightOn} />
+        <ZoomParameter setZoom={setZoom} max={maxZoom} />
+        <div className="rotate-buttons">
+          <RotatableButton
+            handleAxis={setXAxis}
+            imgUrl={rotate}
+            minPx={xLimit.min}
+            maxPx={xLimit.max}
+          />
+          <RotatableButton
+            handleAxis={setYAxis}
+            imgUrl={rotate}
+            minPx={yLimit.min}
+            maxPx={yLimit.max}
           />
         </div>
-        {!isLightOn && <div className="light-overlay"></div>}
-        {!isLightOn && <div className="light-shadow-overlay"></div>}
       </div>
-      <LightButton isOn={isLightOn} toggleIsOn={toggleIsLightOn} />
-      <div className="rotate-buttons">
-        <RotatableButton handleAxis={setXAxis} />
-        <RotatableButton handleAxis={setYAxis} />
-      </div>
+      <RotatableButton
+        handleAxis={setContainerYAxis}
+        imgUrl={wheel}
+        minPx={0}
+        maxPx={4350}
+      />
       <div className="frame-bottom-left"></div>
       <div className="frame-bottom">
         <p>Y-Axis</p>
@@ -48,14 +127,12 @@ export default function Posters() {
   );
 }
 
-function RotatableButton({ handleAxis }) {
+function RotatableButton({ handleAxis, imgUrl, minPx, maxPx }) {
   const [rotation, setRotation] = useState(0);
   const isDragging = useRef(false);
   const center = useRef({ x: 0, y: 0 });
   const prevAngle = useRef(0);
   const buttonRef = useRef(null);
-  const minPx = -200;
-  const maxPx = 200;
   const pxPerDeg = (maxPx - minPx) / 360;
 
   // Given a current angle (degrees)
@@ -123,22 +200,71 @@ function RotatableButton({ handleAxis }) {
   };
 
   return (
-    <div>
+    <div
+      ref={buttonRef}
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
+      onDoubleClick={handleReset}
+      className="rotatable-button"
+    >
+      <img
+        src={imgUrl}
+        alt=""
+        draggable={false}
+        style={{ transform: `rotate(${rotation}deg)` }}
+      />
+    </div>
+  );
+}
+
+function ZoomParameter({ setZoom, max }) {
+  const draggableRef = useRef(null);
+  const isDraggingRef = useRef(false);
+  const startRef = useRef(0);
+
+  const paramHeight = 17.5;
+  const zoomPerDistance = max / paramHeight;
+  const remValue = 16;
+
+  const handleMouseDown = (e) => {
+    if (!draggableRef.current) return;
+    isDraggingRef.current = true;
+    draggableRef.current.style.cursor = "grabbing";
+    const rect = draggableRef.current.getBoundingClientRect();
+    startRef.current = rect.bottom;
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+    e.preventDefault();
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDraggingRef.current) return;
+    const distance = (startRef.current - e.clientY) / remValue;
+    if (distance < paramHeight && distance >= 0) {
+      draggableRef.current.style.bottom = `${distance}rem`;
+      setZoom(distance * zoomPerDistance);
+    }
+  };
+
+  const handleMouseUp = () => {
+    isDraggingRef.current = false;
+    window.removeEventListener("mousemove", handleMouseMove);
+    window.removeEventListener("mouseup", handleMouseUp);
+  };
+
+  return (
+    <div className="zoom-container">
+      <p>ZOOM</p>
+      <div className="zoom-parameter"></div>
       <div
-        ref={buttonRef}
-        onPointerDown={handlePointerDown}
-        onPointerMove={handlePointerMove}
-        onPointerUp={handlePointerUp}
-        onDoubleClick={handleReset}
-        className="rotatable-button"
-      >
-        <img
-          src={rotate}
-          alt=""
-          draggable={false}
-          style={{ transform: `rotate(${rotation}deg)` }}
-        />
-      </div>
+        className="zoom-draggable"
+        ref={draggableRef}
+        onMouseDown={handleMouseDown}
+      ></div>
+      {[...Array(5)].map((_, index) => (
+        <div className="zoom-meter" key={index}></div>
+      ))}
     </div>
   );
 }
